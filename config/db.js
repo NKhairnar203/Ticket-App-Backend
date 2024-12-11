591da9e2d9d9a2d076ca293d9e71825339c138e7
Nilesh Khairnar <nkhairnar543@gmail.com>
2024-12-12 00:04:55 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
-const Connect = mongoose.connect(
-  "mongodb+srv://nkhairnar543:4Uv44Wp7DhWP5N94@cluster0.clk4v.mongodb.net/ticket?retryWrites=true&w=majority&appName=Cluster0"
-);
+const Connect = mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 function connectDB() {
   return Connect.then(() => console.log("Connected to MongoDB...")).catch(
     (err) => console.error(err)
